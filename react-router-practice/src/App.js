43bb95942c023d1cfc3cbd3f451273aca7a50167
Bishwajit <bishwa420@gmail.com>
2020-11-18 18:32:57 +0600

    Very basic routing implemented

--- react-router-practice/src/App.js
+++ react-router-practice/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Category = () => (
+    <div>
+      <h2>Category</h2>
+    </div>
+)
+
+const Products = () => (
+    <div>
+      <h2>Products</h2>
+    </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <nav className="navbar navbar-light">
+            <ul className="navbar navbar-nav">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/category">Category</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+            </ul>
+
+          </nav>
+
+          <Route path="/"><Home/></Route>
+          <Route path="/category"><Category/></Route>
+          <Route path="/products"><Products/></Route>
+        </div>
+    </BrowserRouter>
   );
 }
 
